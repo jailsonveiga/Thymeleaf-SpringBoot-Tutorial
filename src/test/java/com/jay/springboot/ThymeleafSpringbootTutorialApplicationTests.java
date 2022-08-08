65d353cbe3886e4dc26597687a29337776eb6fee
jailsonveiga <jmdaveiga2012@gmail.com>
2022-08-08 19:56:15 -0400

    Initial Commit

---
+++ src/test/java/com/jay/springboot/ThymeleafSpringbootTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jay.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafSpringbootTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
