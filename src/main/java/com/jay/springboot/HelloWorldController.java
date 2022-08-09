0b3db37cf5ff0cd36756b62c5be5886dd065a080
jailsonveiga <jmdaveiga2012@gmail.com>
2022-08-08 20:02:03 -0400

    Get a Request Mapping

--- src/main/java/com/jay/springboot/HelloWorldController.java
+++ src/main/java/com/jay/springboot/HelloWorldController.java
@@ -1,4 +1,15 @@
 package com.jay.springboot;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String Hello(Model model) {
+        model.addAttribute("message", "Hello World");
+        return "HelloWorld";
+    }
 }
